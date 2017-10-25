2761273e94a644440c12bae3ad91e9383b1fe313
Nova Fallen <nfallen@seas.upenn.edu>
2017-10-24 22:55:47 -0400

    expand tests

--- src/char_node_test.cc
+++ src/char_node_test.cc
@@ -3,13 +3,19 @@
 #include "gtest/gtest.h"
 
 namespace {
-class CharNodeTest : public ::testing::Test {
-
-};
 
 TEST(CharNodeTest, ConstructorSetsVars) {
 	CharNode cn('a', 2);
 	EXPECT_EQ('a', cn.get_char());
 	EXPECT_EQ(2, cn.get_freq());
+}
+
+TEST(CharNodeTest, ToProto) {
+	CharNode cn('a', 2);
+	encode_decode::FreqNodeProto proto;
+	cn.to_proto(&proto);
+	EXPECT_EQ(encode_decode::FreqNodeProto_Type_CHAR, proto.type());
+	EXPECT_EQ('a', proto.c());
+	EXPECT_EQ(2, proto.freq());
 }
 }
--- src/inner_node_test.cc
+++ src/inner_node_test.cc
@@ -5,14 +5,66 @@
 
 namespace {
 class InnerNodeTest : public ::testing::Test {
+protected:
+	std::shared_ptr<FreqNode> cn1;
+	std::shared_ptr<FreqNode> cn2;
+	InnerNode in;
 
+	InnerNodeTest() : cn1{new CharNode('a', 1)}, 
+					  cn2{new CharNode('b', 2)},
+					  in(InnerNode(cn1, cn2)) {}
 };
 
-TEST(InnerNodeTest, ConstructorSetsVars) {
-	std::shared_ptr<CharNode> cn1{new CharNode('a', 1)};
-    std::shared_ptr<CharNode> cn2{new CharNode('b', 1)};
-
-    InnerNode in = InnerNode(cn1, cn2);
-	EXPECT_EQ(2, in.get_freq());
+TEST_F(InnerNodeTest, ConstructorSetsVars) {
+	EXPECT_EQ(3, in.get_freq());
 }
+
+TEST_F(InnerNodeTest, LeftAndRightEncodings) {
+	boost::dynamic_bitset<> bits;
+	boost::dynamic_bitset<> bitsa;
+	boost::dynamic_bitset<> bitsb;
+	bitsa.push_back(false);
+	bitsb.push_back(true);
+	std::unordered_map<char, boost::dynamic_bitset<>> encodings;
+	in.encode_node(bits, encodings);
+	auto found_a = encodings.find('a');
+	auto found_b = encodings.find('b');
+	ASSERT_TRUE(found_a != encodings.end());
+	ASSERT_TRUE(found_b != encodings.end());
+	EXPECT_EQ(bitsa, found_a->second);
+	EXPECT_EQ(bitsb, found_b->second);
+}
+
+TEST_F(InnerNodeTest, FindLeftChar) {
+	boost::dynamic_bitset<> bits;
+	bits.push_back(false);
+	char c;
+	size_t index(0);
+	in.find_next_char(bits, index, c);
+	EXPECT_EQ('a', c);
+	EXPECT_EQ(1, index);
+}
+
+TEST_F(InnerNodeTest, FindRightCharIndexNonzero) {
+	boost::dynamic_bitset<> bits;
+	bits.push_back(false);
+	bits.push_back(true);
+	char c;
+	size_t index(1);
+	in.find_next_char(bits, index, c);
+	EXPECT_EQ('b', c);
+	EXPECT_EQ(2, index);
+}
+
+TEST_F(InnerNodeTest, ToProto) {
+	encode_decode::FreqNodeProto proto;
+	in.to_proto(&proto);
+	EXPECT_EQ(encode_decode::FreqNodeProto_Type_INNER, proto.type());
+	EXPECT_EQ(3, proto.freq());
+	EXPECT_EQ('a', proto.lc().c());
+	EXPECT_EQ(1, proto.lc().freq());
+	EXPECT_EQ('b', proto.rc().c());
+	EXPECT_EQ(2, proto.rc().freq());
+}
+
 }
